43df6c3bc479285f01ed43b16a8dbaf5150782d1
hudsun <hudsun1@gmail.com>
2022-02-06 15:54:57 +0800

    Change Wording (Test)

--- Project_Finance_Tracker/finance_tracker/lib/main.dart
+++ Project_Finance_Tracker/finance_tracker/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personl Expenses',
+      title: 'Personal Expenses',
       home: MyHomePage(),
       theme: ThemeData(
         primarySwatch: Colors.green,
